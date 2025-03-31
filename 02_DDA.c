7e59d2ff7b9419824319846dc0ae2d9bbf283b01
Arpan-Saha-25 <arpansaha004@gmail.com>
2025-03-31 13:16:38 +0530

    all quadrant enabled

--- 02_DDA.c
+++ 02_DDA.c
@@ -12,36 +12,65 @@ int x1, y_1, x2, y2;
 
 void DDA(int x1, int y_1, int x2, int y2) {
     printf("\nperforming Digital Differential Analyse.....");
+
     int delX = x2 - x1;
     int delY = y2 - y_1;
-    float slope = delY == 0 ? 0 : (float)delX / delY;
+
+    // float slope = (float)delX / delY;
+
+    int steps = (abs(delX) >= abs(delY)) ? abs(delX) : abs(delY);
+
+    float incX = (float)delX / steps;
+    float incY = (float)delY / steps;
 
     float newX = x1, newY = y_1;
 
-    while (newX <= x2) {
-        printf("\n(x,y) = (%.3f,%.3f)", newX, newY);
-        if (delX >= delY) {
-            // delX = 1;
-            newX = newX + 1;
-            newY = newY + slope;
-        } else if (delX < delY) {
-            // delY = 1;
-            newX = newX + (1 / slope);
-            newY = newY + 1;
-        }
+    glBegin(GL_POINTS);
+    for (int i = 0; i <= steps; i++) {
+        printf("(%.3f,%.3f)\n", (newX + 0.5), (newY + 0.5));
+        glVertex2i((int)(newX + 0.5), (int)(newY + 0.5));
+        newX += incX;
+        newY += incY;
     }
+    glEnd();
+    glFlush();
 
     printf("\nDDA ended.");
 }
 
-int main(int argc, char const *argv[]) {
-    printf("Enter two points...");
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    DDA(x1, y_1, x2, y2);
+}
+
+void init() {
+    glClearColor(0, 0, 0, 1);
+    glColor3f(1, 1, 1);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(-100, 100, -100, 100);  // 100,100
+
+    glPointSize(5);
+}
+
+int main(int c, char **v) {
+    printf("Enter two points...(max100,max100)");
     printf("\nPoint 1 - (x1 y1): ");
     scanf("%d %d", &x1, &y_1);
     printf("Point 2 - (x2 y2): ");
     scanf("%d %d", &x2, &y2);
 
     DDA(x1, y_1, x2, y2);
+
+    glutInit(&c, v);
+    glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
+    glutInitWindowSize(700, 700);
+    glutInitWindowPosition(600, 100);
+    glutCreateWindow("DDA Line Algorithm");
+    init();
+    glutDisplayFunc(display);
+    glutMainLoop();
 
     return 0;
 }
